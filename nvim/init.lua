9b04c6acf9d402f23eb5714d409b6e4707f59e71
Jaimarl <jaimarl.me@gmail.com>
2025-12-02 19:11:06 +0300

    Initial Commit

---
+++ nvim/init.lua
@@ -0,0 +1,13 @@
+-- Configuration 
+require("config.lazy")
+require("config.mappings")
+require("config.options")
+
+
+-- Plugins
+require('plugins.blink')
+require('plugins.bufferline')
+require('plugins.colorizer')
+require('plugins.dashboard')
+require('plugins.lualine')
+require('plugins.nerdy')
---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,5 @@
+require('blink.indent').setup({
+    scope = {
+        highlights = { 'BlinkIndentViolet' }
+    }
+})
